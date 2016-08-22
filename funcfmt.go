4ae5088fbf53a91a8f75471f3299eacbbbb54f3e
Daniel Harrison <daniel.harrison@gmail.com>
2016-08-22 17:41:03 -0400

    Initial import

---
+++ funcfmt.go
@@ -0,0 +1,202 @@
+// Copyright 2016 Daniel Harrison. All Rights Reserved.
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var (
+	wrap      = flag.Int("wrap", 100, "column to wrap at")
+	tab       = flag.Int("tab", 8, "tab width for column calculations")
+	overwrite = flag.Bool("overwrite", false, "overwrite modified files")
+	ignore    = flag.String("ignore", "", "regex matching files to skip")
+)
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		fmt.Println("missing argument: filepath")
+		return
+	}
+
+	root, err := filepath.Abs(flag.Arg(0))
+	if err != nil {
+		fmt.Printf("Error finding absolute path: %s", err)
+		return
+	}
+
+	var ignoreRE *regexp.Regexp
+	if len(*ignore) > 0 {
+		ignoreRE, err = regexp.Compile(*ignore)
+		if err != nil {
+			fmt.Printf("Error compiling ignore regexp: %s", err)
+			return
+		}
+	}
+
+	var diffs int
+	err = filepath.Walk(root, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Error during filesystem walk: %v\n", err)
+			return nil
+		}
+		if ignoreRE != nil && ignoreRE.MatchString(path) {
+			return nil
+		}
+		if fi.IsDir() {
+			return nil
+		}
+		if !strings.HasSuffix(path, ".go") {
+			return nil
+		}
+		pathDiffs, err := checkPath(path)
+		if err != nil {
+			return err
+		}
+		diffs += pathDiffs
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("Error during walk: %s", err)
+		return
+	}
+	if diffs > 0 {
+		fmt.Printf("Found %d diffs\n", diffs)
+		os.Exit(1)
+	}
+}
+
+func checkPath(path string) (int, error) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, path, nil, 0)
+	if err != nil {
+		return 0, err
+	}
+
+	fileBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return 0, err
+	}
+	fileSlice := func(beg token.Pos, end token.Pos) []byte {
+		return fileBytes[fset.Position(beg).Offset:fset.Position(end).Offset]
+	}
+
+	var diffs int
+	var output bytes.Buffer
+	var curFunc bytes.Buffer
+
+	lastPos := token.NoPos
+	for _, d := range f.Decls {
+		if f, ok := d.(*ast.FuncDecl); ok {
+			params := f.Type.Params
+			results := f.Type.Results
+
+			opening := params.Pos() + 1
+			closing := f.Body.Pos() + 1
+			output.Write(fileBytes[fset.Position(lastPos).Offset:fset.Position(opening).Offset])
+			lastPos = closing
+
+			var paramsBuf bytes.Buffer
+			if params != nil {
+				paramsPrefix := ""
+				for _, f := range params.List {
+					paramsBuf.WriteString(paramsPrefix)
+					paramsBuf.Write(fileSlice(f.Pos(), f.End()))
+					paramsPrefix = ", "
+				}
+			}
+			paramsJoined := paramsBuf.Bytes()
+
+			var resultsBuf bytes.Buffer
+			if results != nil {
+				resultsPrefix := ""
+				for _, f := range results.List {
+					resultsBuf.WriteString(resultsPrefix)
+					resultsBuf.Write(fileSlice(f.Pos(), f.End()))
+					resultsPrefix = ", "
+				}
+			}
+			resultsJoined := resultsBuf.Bytes()
+
+			funcMid := `) (`
+			funcEnd := `) {`
+			if results == nil || len(results.List) == 0 {
+				funcMid = `) `
+				funcEnd = `{`
+			} else if len(results.List) == 1 && len(results.List[0].Names) == 0 {
+				funcMid = `) `
+				funcEnd = ` {`
+			}
+
+			curFunc.Reset()
+			colOffset := fset.Position(opening).Column
+			if colOffset+len(paramsJoined)+len(funcMid)+len(resultsJoined)+len(funcEnd) <= *wrap {
+				curFunc.Write(paramsJoined)
+				curFunc.WriteString(funcMid)
+				curFunc.Write(resultsJoined)
+				curFunc.WriteString(funcEnd)
+			} else {
+				if len(params.List) == 0 {
+					// pass
+				} else if *tab+len(paramsJoined) <= *wrap {
+					fmt.Fprintf(&curFunc, "\n\t%s,", paramsJoined)
+				} else {
+					for _, param := range params.List {
+						fmt.Fprintf(&curFunc, "\n\t%s,", fileSlice(param.Pos(), param.End()))
+					}
+				}
+				if *tab+len(funcMid)+len(resultsJoined)+len(funcEnd) <= *wrap {
+					curFunc.WriteByte('\n')
+					curFunc.WriteString(funcMid)
+					curFunc.Write(resultsJoined)
+					curFunc.WriteString(funcEnd)
+				} else {
+					curFunc.WriteString(funcMid)
+					for _, result := range results.List {
+						fmt.Fprintf(&curFunc, "\n\t%s,", fileSlice(result.Pos(), result.End()))
+					}
+					curFunc.WriteByte('\n')
+					curFunc.WriteString(funcEnd)
+				}
+			}
+
+			oldFunc := fileSlice(opening, closing)
+			if !bytes.Equal(oldFunc, curFunc.Bytes()) {
+				prefix := string(fileBytes[fset.Position(d.Pos()).Offset:fset.Position(opening).Offset])
+				fmt.Printf("%s:%d\n", path, fset.Position(d.Pos()).Line)
+				for _, line := range strings.Split(prefix+string(oldFunc), "\n") {
+					fmt.Printf("\x1b[31m-%s\x1b[0m\n", line)
+				}
+				for _, line := range strings.Split(prefix+curFunc.String(), "\n") {
+					fmt.Printf("\x1b[32m-%s\x1b[0m\n", line)
+				}
+				fmt.Print("\n")
+				diffs++
+			}
+			output.Write(oldFunc)
+		}
+	}
+	output.Write(fileBytes[fset.Position(lastPos).Offset:])
+
+	log.Print(*overwrite)
+	if *overwrite {
+		err = ioutil.WriteFile(path, output.Bytes(), 0)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return diffs, nil
+}
